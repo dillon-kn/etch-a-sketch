e030b418d8119e2973319749f22bdba1c0ff07ef
Dillon Nguyen <iamdnguyen1024@gmail.com>
2022-02-21 20:59:24 -0800

    Completed HTML Skeleton

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+An etch-a-sketch implementation with Dr K's eternal eye watching over.
---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+TODO:
+    Implement resolution updater
+    Implement CSS Grid in Container
+    Implement onclick color change
+    Implement Reset
+    Implement eraser vs draw mode
+*/
+
+const updateSliderValue = () => {
+    const slider = document.querySelector(".slider");
+    const sliderLabel = document.querySelector('.slider-label')
+
+    slider.addEventListener('input', (e) => {
+        console.log(e)
+    })
+}
+
+updateSliderValue()
