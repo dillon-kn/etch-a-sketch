1c0573299deed4bbd01a2b20bf7ee0581e74fda1
Dillon Nguyen <iamdnguyen1024@gmail.com>
2022-02-22 21:00:24 -0800

    Implemented grid and resolution updater

--- app.js
+++ app.js
@@ -1,19 +1,68 @@
 /*
 TODO:
-    Implement resolution updater
-    Implement CSS Grid in Container
     Implement onclick color change
+    Implement reset on resolution change
     Implement Reset
     Implement eraser vs draw mode
 */
 
-const updateSliderValue = () => {
+const updateSliderLabel = () => {
+    // On change of slider value, modify the slider label
     const slider = document.querySelector(".slider");
     const sliderLabel = document.querySelector('.slider-label')
 
     slider.addEventListener('input', (e) => {
-        console.log(e)
+        sliderLabel.textContent = `${e.target.value} x ${e.target.value}`;
     })
 }
 
-updateSliderValue()
+const configureGrid = (resolution) => {
+    // Create the CSS grid
+    const gridContainer = document.querySelector('.container');
+
+    // Delete any divs that are in there
+    gridContainer.innerHTML = '';
+
+    // Create a resolution x resolution grid
+    for (let i = 0; i < (resolution ** 2); i++) {
+        const pixel = document.createElement('div');
+        pixel.setAttribute('style', 'border: 1px solid black; border-collapse: true;')
+        gridContainer.appendChild(pixel);
+    }
+
+    // Properly arrange the divs
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${resolution}, 1fr); grid-template-rows: repeat(${resolution}, 1fr);`)
+}
+
+const updateResolution = () => {
+    const slider = document.querySelector('.slider');
+    const gridContainer = document.querySelector('.container');
+
+    slider.addEventListener('input', (e) => {
+        configureGrid(+e.target.value)
+    })
+}
+
+const applyEtchASketch = () => {
+
+}
+
+const main = () => {
+    // Create the CSS Grid with an initial value of 30
+    configureGrid(30);
+    
+    // Create event listener to update slider label
+    updateSliderLabel();
+
+    // Create event listener for updating canvas resolution
+    updateResolution();
+
+    // Create event listener for draw vs erase mode
+
+    // Create event listener for reset button
+
+    // Event listener for each "pixel" in canvas
+    applyEtchASketch();
+}
+
+main()
